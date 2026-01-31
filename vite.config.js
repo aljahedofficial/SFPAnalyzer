320e3c7e279c555889b2e292eef83fe862437dde
Minhaj Al Jahed <aljahedofficial@gmail.com>
2026-01-31 14:55:43 +0000

    Add base path for GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -2,5 +2,6 @@ import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
 // https://vitejs.dev/config/
 export default defineConfig({
+    base: '/SFPAnalyzer/',
     plugins: [react()],
 })
